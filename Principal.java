cc8b99eb4a16f21671306459ae2193b355789c6a
Guilherme D. Sousa <guifgd36@gmail.com>
2024-06-29 15:53:12 -0300

    mais teste

--- Principal.java
+++ Principal.java
@@ -1,7 +1,17 @@
 // Hello World Program
 
-public class Principal {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+    }
+
 }
