f72c2bdb9cccdaee044fde65c1883e6ce007dc06
Guilherme D. Sousa <guifgd36@gmail.com>
2024-07-04 20:13:01 -0300

    Adição da Tela de Login
    Foi adicionado a primeira tela, elementos de CSS e imagens

--- Principal.java
+++ Principal.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
@@ -15,8 +16,12 @@ public class Principal extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Login");
+        primaryStage.setTitle("Crypta Nostalgica");
         primaryStage.show();
+        // bloqueia o redimensionamento da tela
+        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
+        primaryStage.getIcons().add(new Image("images/esboço-logo-export.png"));
     }
 
 }
