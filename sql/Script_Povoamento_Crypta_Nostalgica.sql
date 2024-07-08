445928bd3dc40fa7aaabfcdc10b13700e205c5a4
ThallisNunes <thallisluciano.n@gmail.com>
2024-07-08 19:43:52 -0300

    Adição no Script: Inserção de Generos e Plataaformas

--- sql/Script_Povoamento_Crypta_Nostalgica.sql
+++ sql/Script_Povoamento_Crypta_Nostalgica.sql
@@ -1,1 +1,66 @@
 /* Execute o script a seguir para povoar corretamente o Banco de Dados para a aplicacao.*/
+
+/* Insercao de plataformas em ordem alfabetica */
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('GameBoy', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('GameCube', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('MasterSystem', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('Nintendo 64', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('PC', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('PlayStation 1', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('PlayStation 2', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('PolyStation', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('Super Nintendo', '');
+
+INSERT INTO crypta_nostalgica.Plataforma(nome_plataforma, jogabilidade)
+VALUES('Xbox', '');
+
+/* Insercao de generos em ordem alfabetica */
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Ação');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Aventura');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Corrida');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Estratégia');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Esporte');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Fantasia');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('RPG');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Shooter');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Simulação');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Terror');
+
+INSERT INTO crypta_nostalgica.Genero(nome_genero)
+VALUES('Luta');
